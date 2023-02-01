f607ce26e731f0ac2bf4e5d4f33939a2bc686ea4
titus.dishon <titus.dishon@postcodelottery.co.uk>
2023-02-01 12:39:41 +0300

    fix navbar

--- pages/sections/nav.tsx
+++ pages/sections/nav.tsx
@@ -1,23 +1,76 @@
 import Link from "next/link";
+import React from "react";
 
 export default function NavBar() {
+  const [navbarOpen, setNavbarOpen] = React.useState(false);
+
   return (
-    <ul className="flex w-full items-center space-x-8 sticky bg-gray-100 h-16 top-0 m-0 p-0">
-      <li className="mx-6">
-        <Link href="/">Home</Link>
-      </li>
-      <li className="mx-6">
-        <Link href="/pages/projects">Projects</Link>
-      </li>
-      <li className="mx-6">
-        <Link href="/pages/blogs">Blog</Link>
-      </li>
-      <li className="mx-6">
-        <Link href="/pages/hobbies">Hobbies</Link>
-      </li>
-      <li className="mx-6">
-        <Link href="/pages/contacts">Contacts</Link>
-      </li>
-    </ul>
+    <>
+      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-gray-100">
+        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+            <Link
+              className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase "
+              href="/"
+            >
+              TMD
+            </Link>
+            <button
+              className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+              type="button"
+              onClick={() => setNavbarOpen(!navbarOpen)}
+            >
+              <i className="fas fa-bars"></i>
+            </button>
+          </div>
+          <div
+            className={
+              "lg:flex flex-grow items-center" +
+              (navbarOpen ? " flex" : " hidden")
+            }
+            id="example-navbar-danger"
+          >
+            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+              <li className="nav-item">
+                <Link
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug hover:opacity-75"
+                  href="/pages/projects"
+                >
+                  <i className="fab fa-facebook-square text-lg leading-lg  opacity-75"></i>
+                  <span className="ml-2">Projects</span>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug hover:opacity-75"
+                  href="/pages/blogs"
+                >
+                  <i className="fab fa-facebook-square text-lg leading-lg  opacity-75"></i>
+                  <span className="ml-2">Blogs</span>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug hover:opacity-75"
+                  href="/pages/hobbies"
+                >
+                  <i className="fab fa-facebook-square text-lg leading-lg  opacity-75"></i>
+                  <span className="ml-2">Hobbies</span>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug hover:opacity-75"
+                  href="/pages/contacts"
+                >
+                  <i className="fab fa-facebook-square text-lg leading-lg  opacity-75"></i>
+                  <span className="ml-2">Contacts</span>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 }
